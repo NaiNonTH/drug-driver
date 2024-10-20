c238c380e752b6b1adefeeafc739591ae7a01719
NaiNonTheN00b1 <61338902+NaiNonTH@users.noreply.github.com>
2024-10-20 17:45:29 +0700

    Allow Entities to be centered

--- src/Obstacle.java
+++ src/Obstacle.java
@@ -3,7 +3,7 @@ public class Obstacle extends Entity {
     private boolean isTall;
     
     public Obstacle(String name, int width, int height, int slot, int sceneWidth, int roadWidth, boolean isTall) {
-        super(name, width, height, slot, sceneWidth, roadWidth);
+        super(name, width, height, false, slot, sceneWidth, roadWidth);
         this.isTall = isTall;
     }
 
